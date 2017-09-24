8d199836c9f1c1e4187139e222a1568cab4db1dc
Yossi Cohen <yossi.cohen@live.com>
2017-09-24 17:03:50 +0300

    ref

--- src/api/db.js
+++ src/api/db.js
@@ -1,6 +1,6 @@
 // db.js
 module.exports = () => {
-    const data = { documents: [], text: [], tokens: [], tags: [] }
+    const data = { documents: [], tokens: [] }
 
     const text = 'When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously.\
     “I can tell you very senior CEOs of major American car companies would shake my hand and turn away because I wasn’t worth talking to,” said Thrun, now the co-founder and CEO of online higher education startup Udacity, in an interview with Recode earlier this week.\
