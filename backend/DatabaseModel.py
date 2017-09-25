dc451f375d39aaf486b2654d5cf5079cf9134241
Dima Goldin <solidtim@gmail.com>
2017-09-25 13:46:39 +0300

    added some data models according to readme added tags, filtering on tags added sorting on document listing added spacy tokenizer added option to label tokens added option to retrieve tokens and labels of a document

--- backend/DatabaseModel.py
+++ backend/DatabaseModel.py
@@ -1,16 +1,24 @@
-from elasticsearch  import Elasticsearch
-from elasticsearch_dsl import DocType, Date, Integer, Keyword, Text
+from elasticsearch_dsl import Date, DocType, InnerObjectWrapper, Keyword, Nested, Text, Integer
 from elasticsearch_dsl.connections import connections
 
-connections.create_connection(hosts=['localhost'])
-
+connections.create_connection(hosts = ['localhost'])
 
 
 class Document(DocType):
-  name = Text()
-  body = Text()
+  name = Text(fields={'keyword': Keyword()})
+  text = Text()
   created = Date()
   modified = Date()
+  tags = Keyword()
+
+  def add_tag(self, tag):
+    self.tags.append(tag)
+
+  def add_tags(self, tags):
+    self.tags.extend(tags)
+
+  def remove_tag(self, tag):
+    self.tags.remove(tag)
 
   class Meta:
     index = 'documents'
@@ -18,6 +26,38 @@ class Document(DocType):
 
 
 
+class Tokens(DocType):
+  documentId = Keyword()
+  tokenzier = Keyword()
+  entityTokens = Text()
+
+  class Meta:
+    index = 'tokens'
+
+class Labels(DocType):
+  documentId = Keyword()
+  entityLabels = Keyword()
+
+  class Meta:
+    index = 'labels'
 
 
+class Mention(DocType):
+  mentionId = Keyword()
+  entityId = Keyword()
+  documentId = Keyword()
+  label = Keyword()
+  tokens = Integer()
 
+  class Meta:
+    index = 'mentions'
+
+
+class Entity(DocType):
+  entityId = Keyword()
+  documentId = Keyword()
+  label = Keyword()
+  mentions = Keyword()
+
+  class Meta:
+    index = 'entities'
--- backend/server.py
+++ backend/server.py
@@ -1,77 +1,111 @@
 import uuid
 
 import elasticsearch
+from elasticsearch_dsl import Q
 from flask import Flask, jsonify, request
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
 
 from backend import FileUtils
-from backend.DatabaseModel import Document
+from backend.DatabaseModel import Document, Labels, Tokens
 from backend.FileUtils import allowed_file, get_current_datetime
+from backend.TokenizerWrapper import tokenize
 
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f%Z'
 
 app = Flask(__name__)
 
 
-def hits_to_docs(hits, include_body = None):
+def hits_to_docs(hits, include_text = None):
   result = []
   for hit in hits:
     dict = hit.to_dict()
-    body = {
+    doc = {
       'id': hit.meta.id,
       'name': dict.get('name', ''),
+      'tags': dict.get('tags', []),
       'created': dict.get('created', get_current_datetime()).strftime(DATETIME_FORMAT),
       'modified': dict.get('modified', get_current_datetime()).strftime(DATETIME_FORMAT)
     }
-    if include_body:
-      body['body'] = dict.get('body', '')
-    result.append(body)
+    if include_text:
+      doc['text'] = dict.get('text', '')
+    result.append(doc)
   return result
 
 
-def get_document_internal(document_id):
+def get_document_internal(document_id, include_text = None):
   doc = Document.get(id = document_id)
   dict = doc.to_dict()
 
-  return {
+  doc = {
     'id': doc.meta.id,
     'name': dict.get('name', ""),
+    'tags': dict.get('tags', []),
     'created': dict.get('created', get_current_datetime()).strftime(DATETIME_FORMAT),
     'modified': dict.get('modified', get_current_datetime()).strftime(DATETIME_FORMAT),
-    'body': dict.get('body', ""),
   }
+
+  if include_text:
+    doc['text'] = dict.get('text', '')
+  return doc
 
 
 @app.route('/documents', methods = ['POST'])
 def insert_document():
-  if not 'file' in request.files:
-    return abort(400, 'No selected file')
+  text = ''
+  filename = ''
+  tags = []
 
-  file = request.files['file']
+  if 'file' in request.files:
+    file = request.files['file']
 
-  if file.filename == '':
-    return abort(400, 'No selected file')
-  if file and allowed_file(file.filename):
-    filename = secure_filename(file.filename)
-    text = FileUtils.parse_file(file)
-    new_id = uuid.uuid4()
-    doc = Document(meta = {'id': new_id},
-                   name = filename,
-                   body = text,
-                   created = get_current_datetime(),
-                   modified = get_current_datetime())
-    doc.save()
-    return jsonify(get_document_internal(new_id))
-  return abort(400, "Illegal File Format")
+    if file.filename == '':
+      return abort(400, 'No selected file')
+
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      text = FileUtils.parse_file(file)
+    else:
+      return abort(400, "Illegal File Format")
+
+  if request.is_json:
+    json = request.get_json()
+    text = json.get('text', text)
+    filename = json.get('name', filename)
+    tags = json.get('tags', tags)
+
+  new_id = uuid.uuid4()
+  doc = Document(meta = {'id': new_id},
+                 name = filename,
+                 text = text,
+                 tags = tags,
+                 created = get_current_datetime(),
+                 modified = get_current_datetime())
+  doc.save()
+
+  tokens = tokenize(text)
+
+  tokensDoc = Tokens(meta = {'id': new_id})
+  tokensDoc.entityTokens = tokens
+  tokensDoc.save()
+
+  labels = list()
+  for token in tokens:
+    labels.append("")
+
+  labelsDoc = Labels(meta = {'id': new_id})
+  labelsDoc.entityLabels = labels
+  labelsDoc.save()
+
+  return jsonify(get_document_internal(new_id))
 
 
-@app.route('/documents/<document_id>', methods = ['GET'])
+@app.route('/documents/<string:document_id>', methods = ['GET'])
 def get_document(document_id):
   return jsonify(get_document_internal(document_id))
 
 
-@app.route('/documents/<document_id>', methods = ['PUT'])
+@app.route('/documents/<string:document_id>', methods = ['PUT'])
 def rename_document(document_id):
   doc_name = request.args.get('name')
   doc = Document.get(id = document_id)
@@ -82,7 +116,7 @@ def rename_document(document_id):
   return jsonify(get_document_internal(document_id))
 
 
-@app.route('/documents/<document_id>', methods = ['DELETE'])
+@app.route('/documents/<string:document_id>', methods = ['DELETE'])
 def delete_document(document_id):
   try:
     Document(meta = {'id': document_id}).delete()
@@ -92,64 +126,97 @@ def delete_document(document_id):
 
 
 @app.route('/documents', methods = ['GET'])
-def get_documents(document_id):
-  request.args.get('sortBy')
-  request.args.get('order')
-  request.args.get('page')
-  request.args.get('pageSize')
+def get_documents():
+  sortBy = request.args.get('sortBy')
+  order = request.args.get('order','desc')
+  page = request.args.get('page')
+  pageSize = request.args.get('pageSize')
+  tags = request.args.getlist('tags')
 
-  hits = Document.search().scan()
+  search = Document.search()
+  for tag in tags:
+    search = search.filter('term', tags=tag)
+
+  if sortBy:
+    if sortBy == 'name':
+      sortBy = sortBy+".keyword"
+    search = search.sort(
+      {sortBy: {"order": order}}
+    )
+
+  hits = search.execute()
   result = hits_to_docs(hits)
   return jsonify(result)
 
 
 @app.route('/documents/search', methods = ['GET'])
 def search_document():
-  query = request.args.get('query')
-  is_search_text = request.args.get('searchText')
-  page = request.args.get('page')
-  page_size = request.args.get('pageSize')
+  query = request.args.get('query', '')
+  is_search_text = request.args.get('searchText', '')
+  page = request.args.get('page', 0)
+  page_size = request.args.get('pageSize', 100)
 
   fields = ["name"]
   if is_search_text and is_search_text.lower() == "True".lower():
-    fields.append("body")
+    fields.append("text")
 
-  hits = Document.search().query("query_string", fields = fields, query = query).execute()
-  result = hits_to_docs(hits, include_body = True)
+  if len(request.args) == 0:
+    hits = Document.search().query("match_all").execute()
+  else:
+    hits = Document.search().query("query_string", fields = fields, query = query).execute()
+  result = hits_to_docs(hits, include_text = True)
   return jsonify(result)
 
 
-@app.route('/documents/<document_id>/text', methods = ['GET'])
+@app.route('/documents/<string:document_id>/text', methods = ['GET'])
 def get_document_text(document_id):
-  doc = get_document_internal(document_id)
-  return jsonify(doc['body'])
+  doc = get_document_internal(document_id, include_text = True)
+  return jsonify(doc['text'])
 
 
-@app.route('/documents/<document_id>/tokens', methods = ['GET'])
+@app.route('/documents/<string:document_id>/tokens', methods = ['GET'])
 def get_document_tokens(document_id):
-  pass
+  tokens = Tokens.get(id=document_id)
+  tokenList = list()
+  tokenList.extend(tokens['entityTokens'])
+  return jsonify(tokenList)
 
 
-@app.route('/documents/<document_id>/entities/labels', methods = ['GET'])
+@app.route('/documents/<string:document_id>/entities/labels', methods = ['GET'])
 def get_document_entity_labels(document_id):
-  pass
+  labels = Labels.get(id=document_id)
+  labelList = list()
+  labelList.extend(labels['entityLabels'])
+  return jsonify(labelList)
 
 
-@app.route('/documents/<document_id>/<token_index>/entities/labels', methods = ['GET'])
+@app.route('/documents/<string:document_id>/<int:token_index>/entities/labels', methods = ['GET'])
 def get_token_entity_labels(document_id, token_index):
-  pass
+  labels = Labels.get(id = document_id)
+  return labels.entityLabels[token_index]
 
 
-@app.route('/documents/<document_id>/<token_index>/entities/labels', methods = ['PUT'])
+@app.route('/documents/<string:document_id>/<int:token_index>/entities/labels', methods = ['PUT'])
 def set_token_entity_labels(document_id, token_index):
-  request.args.get('label')
-  pass
+  label = request.args.get('label','NA')
+
+  labels = Labels.get(id = document_id, ignore=404)
+  if not labels:
+    labels = Labels(meta = {'id': document_id}, entityLabels = [])
+  labels['entityLabels'][token_index] = label
+  labels.save()
+  return "OK"
 
 
 @app.route('/documents/<document_id>/<token_index>/entities/labels', methods = ['DELETE'])
 def delete_token_entity_labels(document_id, token_index):
-  request.args.get('label')
-  pass
+
+  labels = Labels.get(id = document_id, ignore=404)
+  if not labels:
+    labels = Labels(meta = {'id': document_id}, entityLabels = [])
+  labels['entityLabels'][token_index] = ""
+  labels.save()
+  return "OK"
 
 
 @app.route('/documents/<document_id>/entities', methods = ['GET'])
@@ -158,10 +225,12 @@ def get_document_entities(document_id):
 
 
 @app.route('/documents/<document_id>/entities/mentions', methods = ['GET'])
-def get_document_entities(document_id):
+def get_document_mentions(document_id):
   pass
 
 
 if __name__ == '__main__':
   Document.init()
+  Labels.init()
+  Tokens.init()
   app.run(port = 12000, debug = True)
